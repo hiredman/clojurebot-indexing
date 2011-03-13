31cfafb4289787bf62f80c1a39b54a1ce251da08
Kevin Downey (hiredman) <redchin@gmail.com>
2011-03-12 17:54:51 -0800

    more movement towards configurable host:port

--- src/clojurebot/indexing.clj
+++ src/clojurebot/indexing.clj
@@ -6,24 +6,29 @@
 
 (def *host* "localhost")
 
+(def *port* 9200)
+
+(defn host-port []
+  (str *host* ":" *port*))
+
 (defn index* [place time doc]
-  (http/put (format "http://%s:9200/irc/msg/%s-%s"
-                    *host* place time)
+  (http/put (format "http://%s/irc/msg/%s-%s"
+                    (host-port) place time)
             {:body (json/encode doc)}))
 
 (def page-size 4)
 
 (defn query [string page]
-  (->> (http/get (format "http://%s:9200/irc/_search"
-                         *host*)
-                 {:query-params {:q string
-                                 :size 4
-                                 :from (* page page-size)
-                                 :sort "time:desc"}})
-       :body
-       json/decode
-       :hits
-       :hits
+  (->> (format "http://%s/irc/_search" (host-port))
+       (http/get {:query-params
+                  {:q string
+                   :size 4
+                   :from (* page page-size)
+                   :sort "time:desc"}})
+       (:body)
+       (json/decode)
+       (:hits)
+       (:hits)
        (map :_source)))
 
 (defn index [bag]
@@ -39,14 +44,13 @@
 (defn search [{:keys [message]}]
   (let [x (.replaceAll message "^search for " "")
         brack (.lastIndexOf x "[")
-        x1 (if (pos? brack)
-             (.trim (subs x 0 (dec brack)))
-             x)
+        qstring (if (pos? brack)
+                  (.trim (subs x 0 (dec brack)))
+                  x)
         page (if (pos? brack)
                (Long/parseLong (subs x (inc brack) (.lastIndexOf x "]")))
                0)
-        r (query x1 page)
-        r (for [{:keys [channel sender time message]} r]
+        r (for [{:keys [channel sender time message]} (query qstring page)]
             (format "<%s:%s> %s" channel sender message))]
     (when (seq r)
       (apply str (interpose \newline r)))))
