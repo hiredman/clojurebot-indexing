47289ba9b302e0bfb9e060b889864b37826cd7b5
Kevin Downey (hiredman) <redchin@gmail.com>
2011-03-14 09:26:56 -0700

    make the query function actually work

--- src/clojurebot/indexing.clj
+++ src/clojurebot/indexing.clj
@@ -19,17 +19,17 @@
 (def page-size 4)
 
 (defn query [string page]
-  (->> (format "http://%s/irc/_search" (host-port))
-       (http/get {:query-params
-                  {:q string
-                   :size 4
-                   :from (* page page-size)
-                   :sort "time:desc"}})
-       (:body)
-       (json/decode)
-       (:hits)
-       (:hits)
-       (map :_source)))
+  (-> (format "http://%s/irc/_search" (host-port))
+      (http/get {:query-params
+                 {:q string
+                  :size 4
+                  :from (* page page-size)
+                  :sort "time:desc"}})
+      (:body)
+      (json/decode)
+      (:hits)
+      (:hits)
+      ((partial map :_source))))
 
 (defn index [bag]
   (let [{:keys [channel time] :as msg}
